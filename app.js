0435ba53e80f7743aea9d411dbe20f0602a83809
ElkinVera17 <eavera6@espe.edu.ec>
2026-07-21 21:36:56 -0500

    feat: research

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 
 document.addEventListener("DOMContentLoaded", iniciaPagina);
+let productosjson = [];
 let usuarios = [];          // API
 let usuariosLocales = [];   // LocalStorage
 document.getElementById("btnCrearUsuario").addEventListener("click", agregarUsuario);
@@ -9,17 +10,27 @@ document.getElementById("btnCrearUsuario").addEventListener("click", agregarUsua
 const btnProductos = document.getElementById("btnCargarProductos");
 btnProductos.addEventListener("click", cargarProductos);
 
+
+const btnCatFact = document.getElementById("btnCatFact");
+btnCatFact.addEventListener("click", cargarCatFact);
 //Perfile
 async function fotoPerfil() {
+
+
     const img = document.getElementById("imagenPerfil");
     const estado = document.getElementById("estadoImagen");
+    try {
+        const respuesta = await fetch("https://dog.ceo/api/breeds/image/random");
+        const perfil = await respuesta.json();
 
-    const respuesta = await fetch("https://dog.ceo/api/breeds/image/random");
-    const perfil = await respuesta.json();
+        estado.textContent = "Renderizando pixeles..."
+        img.src = perfil.message;
+        estado.textContent = "Listo!"
 
-    estado.textContent = "Renderizando pixeles..."
-    img.src = perfil.message;
-    estado.textContent = "Listo!"
+    } catch (error) {
+        estado.textContent = "Error loading profile image.";
+    }
+
 }
 
 //Inicio de pagina
@@ -41,13 +52,21 @@ async function CargarUsuarios() {
 
     const estado = document.getElementById("estadoTabla");
 
+     try {
+
     const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
     usuarios = await respuesta.json();
 
     const contenedor = document.getElementById("contenedorTabla");
 
     mostrarUsuarios();
-    estado.textContent = "success";
+    estado.textContent = "Unete¡";
+
+    } catch (error) {
+
+        estado.textContent = "Error loading users.";
+
+    }
 
 }
 
@@ -89,7 +108,8 @@ function mostrarUsuarios() {
     const listaCompleta = [...usuarios, ...usuariosLocales];
 
     let tabla = `
-        <table class="table table-bordered table-striped">
+    <div class="table-responsive">
+       <table class="table table-bordered table-striped align-middle">
 
             <thead class="table-dark">
                 <tr>
@@ -119,9 +139,11 @@ function mostrarUsuarios() {
     });
 
     tabla += `
-            </tbody>
-        </table>
-    `;
+    </tbody>
+</table>
+
+</div>
+`;
 
     document.getElementById("contenedorTabla").innerHTML = tabla;
 
@@ -130,27 +152,37 @@ function mostrarUsuarios() {
 //Products
 
 async function cargarProductos() {
-    const estado=document.getElementById("estadoProductos");
+    const estado = document.getElementById("estadoProductos");
+
+    try {
     const respuesta = await fetch("https://dummyjson.com/products");
     const productos = await respuesta.json();
+
+    productosjson = productos.products;
 
     mostrarProductos(productos.products);
 
     estado.textContent = "Nuevo calago bajos precios¡";
+     } catch (error) {
+
+        estado.textContent = "Error loading products.";
+
+    }
 }
 
 function mostrarProductos(productos) {
 
     productos.forEach(producto => {
-        contenedorProductos.innerHTML += 
-        `<div class ="col-md-4 col-lg-3" >
-            <div class="card" >
+        contenedorProductos.innerHTML +=
+            `<div class ="col-md-4 col-lg-3" >
+            <div class="card product-card" >
                 <h5 class="card-title">${producto.title}</h5>
                 <img src="${producto.thumbnail}" class="card-img-top" alt="${producto.title}">
                 <div class="card-body">
                     <p class="card-text">${producto.category}</p>
                     <p class="card-text">${producto.description}</p>
                      <p class="card-text">${producto.price}</p>
+                     <button class="btn-comprar" data-id="${producto.id}">Comprar</button>
                 </div>
         </div>`
 
@@ -158,4 +190,55 @@ function mostrarProductos(productos) {
     document.getElementById("contenedorProductos").innerHTML = cards;
 }
 
+async function cargarCatFact() {
+
+    const alerta = document.getElementById("catFact");
+
+    try {
+    const respuesta = await fetch("https://catfact.ninja/fact");
+    const estado = document.getElementById("estadoFact");
+    const dato = await respuesta.json();
+    alerta.textContent = dato.fact;
+    alerta.classList.remove("d-none");
+    estado.textContent = "WOW!";
+
+     } catch (error) {
+        alerta.textContent = "Error loading cat fact.";
+        alerta.classList.remove("d-none");
+        alerta.classList.replace("alert-info", "alert-danger");
+    }
+}
+
+document
+    .getElementById("btnVerJSONUsuarios")
+    .addEventListener("click", verJSONUsuarios);
+function verJSONUsuarios() {
+
+    document.getElementById("contenidoJSON").textContent =
+        JSON.stringify(usuarios, null, 2);
+
+    const modal = new bootstrap.Modal(
+        document.getElementById("modalJSON")
+    );
+
+    modal.show();
+
+}
+
+document
+    .getElementById("btnVerJSONProductos")
+    .addEventListener("click", verJSONProductos);
+
+function verJSONProductos() {
+
+    document.getElementById("contenidoJSON").textContent =
+        JSON.stringify(productosjson, null, 2);
+
+    const modal = new bootstrap.Modal(
+        document.getElementById("modalJSON")
+    );
+
+    modal.show();
+
+}
 
